6c8a13ebb845b3c9a5fa9d56136b093d1df1e76f
virijevicj <89025058+virijevicj@users.noreply.github.com>
2021-09-07 21:46:56 +0200

    Create resenje.sql

---
+++ Nedelja05/Sreda/resenje.sql
@@ -0,0 +1,24 @@
+--zadatak1
+SELECT * 
+FROM profesori
+WHERE godine > 45;
+
+--zadatak2
+SELECT ime, prezime
+FROM ucenici
+WHERE godine > 15 AND prosek > 6.5;
+
+--zadatak3
+SELECT godine
+FROM ucenici
+ORDER BY godine DESC;
+
+--zadatak4
+SELECT *
+FROM profesori
+WHERE predmet LIKE 'Informatika';
+
+--zadatak5
+SELECT ucenikid, ime, prezime
+FROM ucenici
+WHERE ime LIKE '______';
